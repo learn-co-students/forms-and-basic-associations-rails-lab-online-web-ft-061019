8703943b283698462ed81300c4d9b47b502dca1e
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-08-22 23:00:58 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,10 +7,18 @@ class Song < ActiveRecord::Base
       self.genre = Genre.find_or_create_by(name: name)
   end
 
+  def genre_name
+    self.genre ? self.genre.name : nil
+  end 
+
   def artist_name=(name)
       self.artist = Artist.find_or_create_by(name: name)
   end
 
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+  
   def note_contents=(content)
       self.note = Note.find_or_create_by(content: content)
   end
